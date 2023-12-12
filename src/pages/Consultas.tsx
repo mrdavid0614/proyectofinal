9661bbf65c3e5c50719d4539bfc5b8191cdde615
Raymond Mora <raymondinf23@gmail.com>
2023-12-12 04:33:34 -0400

    Add search functionality and filter data based on search input

--- src/pages/Consultas.tsx
+++ src/pages/Consultas.tsx
@@ -1,12 +1,46 @@
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonLabel, IonPage, IonSearchbar, IonSegment, IonSegmentButton, IonText, IonTitle, IonToolbar } from '@ionic/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Driver } from '../types/driver';
 import { Vehicle } from '../types/vehicle';
 import './Consultas.css';
 
+const info: { vehicle: Vehicle[], driver: Driver[] } = {
+  vehicle: [
+    {
+      plate: 'A123456',
+      owner: '1',
+    }
+  ],
+  driver: [
+    {
+      license: '1',
+      name: 'Fulano de tal',
+      phone: '809-999-9999',
+      address: 'La casita',
+      photo: 'https://cdn-icons-png.flaticon.com/512/4792/4792929.png',
+      birthdate: new Date().toLocaleDateString(),
+    }
+  ],
+}
+
 const Consultas: React.FC = () => {
   const [currentView, setCurrentView] = useState<'vehicle' | 'driver'>('vehicle');
+  const [search, setSearch] = useState('');
   const [data, setData] = useState<(Vehicle[] | Driver[])>([]);
+
+  useEffect(() => {
+    if (!search) return;
+
+    const filteredData = currentView === 'vehicle' ? 
+      info[currentView].filter(vehicle => vehicle.plate.toLowerCase().includes(search.toLowerCase()))
+      : info[currentView].filter(driver => driver.license.toLowerCase().includes(search.toLowerCase()));
+
+    setData(filteredData);
+  }, [search]);
+
+  useEffect(() => {
+    setData([]);
+  }, [currentView]);
 
   return (
     <IonPage>
@@ -22,7 +56,7 @@ const Consultas: React.FC = () => {
           </IonToolbar>
         </IonHeader>
 
-        <IonSegment value="vehicle" onIonChange={(e) => setCurrentView(e.detail.value as typeof currentView ?? 'vehicle')}>
+        <IonSegment defaultValue="vehicle" onIonChange={(e) => setCurrentView(e.detail.value as typeof currentView ?? 'vehicle')}>
           <IonSegmentButton value="vehicle">
             <IonLabel>Vehículo</IonLabel>
           </IonSegmentButton>
@@ -31,7 +65,7 @@ const Consultas: React.FC = () => {
           </IonSegmentButton>
         </IonSegment>
 
-        <IonSearchbar className="ion-margin-top" placeholder={`Buscar por ${currentView === 'vehicle' ? 'placa' : 'licencia'}`} />
+        <IonSearchbar className="ion-margin-top" placeholder={`Buscar por ${currentView === 'vehicle' ? 'placa' : 'licencia'}`} onIonChange={(e) => setSearch(e.detail.value as string)} />
 
         {(currentView === 'vehicle' && !!data.length) && (
           <section>
@@ -56,7 +90,7 @@ const Consultas: React.FC = () => {
               <IonCard key={driver.license}>
                 <IonCardHeader>
                     <IonCardTitle>{driver.name}</IonCardTitle>
-                    <IonCardSubtitle>{Intl.DateTimeFormat('es-do').format(new Date(driver.birthdate))}</IonCardSubtitle>
+                    <IonCardSubtitle>{driver.birthdate && Intl.DateTimeFormat('es-do').format(new Date(driver.birthdate))}</IonCardSubtitle>
                 </IonCardHeader>
         
                 <IonCardContent>
